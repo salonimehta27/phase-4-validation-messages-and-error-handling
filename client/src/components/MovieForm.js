0d79087d4675e4c6341daeda7131df4da501725d
Saloni <salonimehta2127@gmail.com>
2021-11-06 21:08:28 -0400

    Completed

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([])
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -13,18 +14,33 @@ function MovieForm() {
     discount: false,
     female_director: false,
   });
-
-  function handleSubmit(e) {
+  // make the function async to enable the await keyword
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    // fetch returns a Promise, we must await it
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    });
+    // response.json() returns a Promise, we must await it
+    const data = await response.json()
+    if (response.ok) {
+      console.log("Movie Created", data)
+    }
+    else {
+      setErrors(data.errors)
+    }
+    //   .then((response) => {
+    //   if (response.ok) {
+    //     response.json().then((newMovie) => console.log(newMovie))
+    //   }
+    //   else {
+    //     response.json().then((errorData) => setErrors(errorData.errors))
+    //   }
+    // })
   }
 
   function handleChange(e) {
@@ -38,6 +54,16 @@ function MovieForm() {
 
   return (
     <Wrapper>
+      {
+        errors.length > 0 && (
+          <ul style={{ color: 'red' }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+
+          </ul>
+        )
+      }
       <form onSubmit={handleSubmit}>
         <FormGroup>
           <label htmlFor="title">Title</label>
